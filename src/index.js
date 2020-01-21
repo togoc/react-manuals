1ba3d4c1c29ff3e04f9e0e9aca3c393b5f334364
togoc <oncwnuDRPW2KUww1bxVRTJZJIaq0@git.weixin.qq.com>
2020-01-21 22:57:25 +0800

    dom 加载原理

--- src/index.js
+++ src/index.js
@@ -1,12 +1,84 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 创建简单的react元素
+// const app = <h1>welcome</h1>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//原理
+// const createApp = (props) => {
+//     return (
+//         <h1>welcome {props.title}</h1>
+//     )
+// }
+// const app = createApp({
+//     title: '16.12.0'
+// })
+
+// ReactDOM.render(
+//     app,
+//     document.querySelector("#root")
+// )
+
+
+//函数标签的形式 标签开头要大写
+// const App = (props) => {
+//     return (
+//         <h1>welcome {props.title}</h1>
+//     )
+// }
+
+
+
+
+// 类形式渲染原理
+// class App extends React.Component {
+//     render() {
+//         console.log(this.props)
+//         const { title } = this.props
+//         return (
+//             <h1>welcome {title}</h1>
+//         )
+//     }
+// }
+// const app = new App({
+//     title: '16.12.0',
+//     name: 'togoc'
+// }).render()
+
+
+//jsx原理
+// const dom = {
+//     tag: 'div',
+//     attrs: {
+//         className: 'app',
+//         id: 'app'
+//     },
+//     children: [{
+//         tag: 'h1',
+//         children: ['这是一个h1标签']
+//     }]
+// }
+class App extends React.Component {
+    render() {
+        return (
+            React.createElement(
+                'div', {
+                className: 'app',
+            },
+                '这是一个div标签',
+                React.createElement(
+                    'h1', {
+
+                },
+                    '这是一个h1标签'
+                )
+            )
+        )
+    }
+}
+
+ReactDOM.render(
+    <App title="16.12.0" />, //相当于自动执行了App里的render
+    // app,
+    document.querySelector("#root")
+)
