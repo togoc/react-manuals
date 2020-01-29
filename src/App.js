aecd96990905d4b82c9ef9be70ad8ca09cefd364
togoc <oncwnuDRPW2KUww1bxVRTJZJIaq0@git.weixin.qq.com>
2020-01-29 22:22:43 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 import { increment, decrement } from './action/cart'
-
-export default class App extends Component {
+import { connect } from 'react-redux'
+class App extends Component {
     state = {
         cart: []
     }
     getState = () => {
         this.setState((state) => {
-            return this.props.store.getState() //{cart:[]}
+            let { cart } = this.props
+            return { cart }
         })
     }
     componentDidMount() {
         this.getState()
-        this.props.store.subscribe(this.getState) //修改数据后重新渲染
+        // this.props.store.subscribe(this.getState) //修改数据后重新渲染
     }
     render() {
-        const { dispatch } = this.props.store
+        const { decrement, increment } = this.props
+        console.log(this.props)
         return (
             <table border='1' cellSpacing="0">
                 <thead>
@@ -33,11 +35,11 @@ export default class App extends Component {
                                 <td>{v.price}</td>
                                 <td style={{ display: 'flex', justifyContent: 'space-around' }}>
                                     <button onClick={() => {
-                                        dispatch(decrement(v.id))
+                                        decrement(v.id)
                                     }}>-</button>
                                     {v.amount}
                                     <button onClick={() => {
-                                        dispatch(increment(v.id))
+                                        increment(v.id)
                                     }}>+</button>
                                 </td>
                             </tr>
@@ -46,5 +48,20 @@ export default class App extends Component {
                 </tbody>
             </table>
         )
+    }
+}
+
+//connect传递的内容都会传到目标的props
+export default connect((state) => {
+    console.log(state)
+    return state
+}, { increment, decrement })(App)
+
+//第二个参数等价于 
+dispatch => {
+    return {
+        increment: (id) => dispatch(increment(id))
+        ,
+        decrement: (id) => dispatch(decrement(id))
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './App'
 
 import store from './store'
-console.log(store)
 window.store = store
 
 ReactDOM.render(
-    <App store={store} />
+    <Provider store={store}>
+        <App />
+    </Provider>
     ,
     document.querySelector('#root')
 )
